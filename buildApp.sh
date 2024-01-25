88abcf0c2eff11e301720cb390c70f6e8df34d60
Cole J Radetich <cole@radi.tools>
2024-01-25 06:40:10 -0700

    add: build script

---
+++ buildApp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew update
+brew install coreutils make pkg-config tehzz/n64-dev/mips64-elf-binutils glew glfw mingw-w64
+
+gmake
+
+#mv build/jp_pc Mariarm
+#mv build/eu_pc Mariarm
+mv build/us_pc Mariarm
+mv Mariarm/sm64.us.f3dex2e Mariarm/Mariarm
+mv Mariarm Mariarm.app
+cp Icon? Mariarm/
